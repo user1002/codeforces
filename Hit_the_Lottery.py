326e0d29dafc20fbc03278584e6edb88957174fe
Joseph Thekale <josepht1002@gmail.com>
2020-04-23 00:35:47 -0400

    Update Hit_the_Lottery.py

--- Hit_the_Lottery.py
+++ Hit_the_Lottery.py
@@ -6,7 +6,6 @@ amt=int(amt)
 bills = [20,10,5,1] #$ bills, no need for $100 (see below)
 count=0 #count for bills
 
-
 if amt >= 100:
 	a=divmod(amt,100) #get quotient and remainder
 	amt=a[1]  #remainder

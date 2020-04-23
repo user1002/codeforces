1300f4897188d05a49757d6fffcc865532b8a148
Joseph Thekale <josepht1002@gmail.com>
2020-04-23 00:40:18 -0400

    Update Hit_the_Lottery.py

--- Hit_the_Lottery.py
+++ Hit_the_Lottery.py
@@ -1,15 +1,15 @@
 #Problem: https://codeforces.com/problemset/problem/996/A
 
 amt = input() #capture input
-amt=int(amt)
+amt = int(amt)
 
 bills = [20,10,5,1] #$ bills, no need for $100 (see below)
-count=0 #count for bills
+count = 0 #count for bills
 
 if amt >= 100:
-	a=divmod(amt,100) #get quotient and remainder
-	amt=a[1]  #remainder
-	count=a[0] #quotient
+	a = divmod(amt,100) #get quotient and remainder
+	amt = a[1]  #remainder
+	count = a[0] #quotient
 
 for b in bills: #for remaning money loop through bill types
 	while b <= amt: #if divisible by bill
